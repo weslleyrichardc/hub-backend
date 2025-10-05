b6bdfce11de964293daac4b1b5edb5a2318a9574
Weslley Richard <weslleyrichardc@gmail.com>
2025-10-05 12:09:09 -0300

    feat: Login, Register, Logout

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix("auth")
+    ->name("auth.")
+    ->group(function () {
+        Route::post("/user", [AuthController::class, "register"])->name("user");
+        Route::post("/login", [AuthController::class, "login"])->name("login");
+        Route::post("/logout", [AuthController::class, "logout"])->name(
+            "logout",
+        );
+    });
+
+Route::middleware("auth:sanctum")->group(function () {
+    Route::get("/user", fn() => auth()->user())->name("user");
+});
